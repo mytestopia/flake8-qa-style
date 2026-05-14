f92d00d207b7c5e0e3214b5a40a264326ea1c19c
Пышкин Никита Сергеевич <n.pyshkin@2gis.ru>
2026-05-14 23:41:21 +0300

    some renaming

--- flake8_qa_style/checkers/node_visitors/async_visitor.py
+++ flake8_qa_style/checkers/node_visitors/async_visitor.py
@@ -4,18 +4,18 @@ from flake8_qa_style.checkers.node_visitors._node_visitor import NodeVisitor
 from flake8_qa_style.errors import AsyncFunctionWithoutAsynchronous
 
 
-class _AwaitFinder(ast.NodeVisitor):
+class _AsynchronousFinder(ast.NodeVisitor):
     def __init__(self):
-        self.has_await = False
+        self.has_async = False
 
     def visit_Await(self, _node: ast.Await):
-        self.has_await = True
+        self.has_async = True
 
     def visit_AsyncWith(self, _node: ast.AsyncWith):
-        self.has_await = True
+        self.has_async = True
 
     def visit_AsyncFor(self, _node: ast.AsyncFor):
-        self.has_await = True
+        self.has_async = True
 
     def visit_AsyncFunctionDef(self, _node: ast.AsyncFunctionDef):
         pass  # don't descend into nested async functions
@@ -24,11 +24,11 @@ class _AwaitFinder(ast.NodeVisitor):
 class AsyncVisitor(NodeVisitor):
 
     def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef):
-        finder = _AwaitFinder()
+        finder = _AsynchronousFinder()
         for child in ast.iter_child_nodes(node):
             finder.visit(child)
 
-        if not finder.has_await:
+        if not finder.has_async:
             self.error_from_node(AsyncFunctionWithoutAsynchronous, node, func_name=node.name)
 
         self.generic_visit(node)
